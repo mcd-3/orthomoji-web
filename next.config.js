febb2cd91c6fd891b8b02bbe95e44176ab2f0708
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2023-07-28 15:34:34 -0400

    Revert

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
-// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
-module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-}
+module.exports = nextConfig
