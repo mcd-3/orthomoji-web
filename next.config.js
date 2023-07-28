67e0e2cbbfbe3ecafa1283931a223e51db4268a0
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2023-07-28 16:04:21 -0400

    Fix config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+if (isGithubActions) {
+    nextConfig.output = 'export';
+}
 
 module.exports = nextConfig
