5c5d10bd1cb5e582b32c74868f5f68fccf9e201a
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2023-08-02 19:10:38 -0400

    Fixed collapse content styles

--- src/app/components/collapseContent.js
+++ src/app/components/collapseContent.js
@@ -1,7 +1,11 @@
 import { useCollapse } from 'react-collapsed'
+import Image from 'next/image';
 
 import styles from './../styles/components/collapse-content.module.css';
 import { nunito } from '../assets/fonts.js';
+
+import arrowDown from './../assets/arrow-down.svg';
+import arrowRight from './../assets/arrow-right.svg';
 
 export default function CollapseContent({
   children,
@@ -16,7 +20,25 @@ export default function CollapseContent({
         className={styles["expand-button"]}
         {...getToggleProps()}
       >
-        <div>
+        <div className={styles["span-container"]}>
+          <span className={styles["image-span"]}>
+            {isExpanded
+              ?
+                <Image
+                  className={styles.icon}
+                  src={arrowDown}
+                  width={32}
+                  height={32}
+                />
+              :
+                <Image
+                  className={styles.icon}
+                  src={arrowRight}
+                  width={32}
+                  height={32}
+                />
+            }
+          </span>
           <span className={`${styles["text-span"]} ${nunito}`}>{isExpanded ? expandedText : collapsedText}</span>
         </div>
       </button>
